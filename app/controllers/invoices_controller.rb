690b7f20acee628aafc1bd2991ac9938664a435c
Mei Yang <mei@hnry.io>
2022-05-31 11:21:07 +1200

    add calculating invoice_total in controller and method called invoice_number in Invoice-model

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,13 +7,24 @@ class InvoicesController < ApplicationController
         @invoice = Invoice.new
     end
     def create
-      @invoice = Invoice.new(invoice_params)
-      if @invoice.save
-          redirect_to invoices_path
-        else
-          render 'new'
-        end
+        @invoice = Invoice.new(invoice_params)
+       
+        @invoice.invoices_total = @invoice.price * @invoice.quantity
+           if @invoice.save
+                 redirect_to invoices_path
+           else
+              render 'new'
+           end
     end
+    
+    # test = Invoice.create
+    # test.id #=> 1
+    # test.invoices_id #=> "INV00001"
+    
+    # test2 = Invoice.create
+    # test2.id #=> 2
+    # test2.invoices_id #=> "INV00002"
+   
     def show
         @invoice = Invoice.find(params[:id])
     end
@@ -22,6 +33,6 @@ class InvoicesController < ApplicationController
     end
     private 
   def invoice_params 
-    params.require(:invoice).permit(:client_name,:invoice_id, :invoice_item, :quantity, :invoices_total,:expence_date) 
+        params.require(:invoice).permit(:client_name,:invoice_id, :price, :invoice_item, :quantity, :invoices_total,:expence_date) 
   end
 end
